6377cdad4943d3b372cf9dd69a7badd5b3b18e10
Dewtalo <rykane0519@gmail.com>
2023-11-03 19:59:11 +0900

    基本的に全機能

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -2,11 +2,74 @@
 
     <h5 class="text-center text-5xl font-bold py-3">Users</h5>
 
+    <div class="search">
+        <form action="{{ route('users') }}" method="GET">
+            @csrf
 
+            <div class="form-group">
+                <div>
+                    <label for="">Type a destination
+                    <div>
+                        <input type="text" name="about_me" value="{{ $search_about_me }}">
+                    </div>
+                    </label>
+                </div>
+
+                <div>
+                    <label for="">Category
+                    <div>
+                        <select name="category" data-toggle="select">
+                            <option value="guide" @if($search_category == "guide") selected @endif>Guide</option>
+                            <option value="tourist" @if($search_category == "tourist") selected @endif>Tourist</option>
+                        </select>
+                    </div>
+                    </label>
+                </div>
+
+                <div>
+                    <label for="">
+                    <div>
+                        <select name="place" data-toggle="select">
+            
+                        @foreach($places as $place)
+                            <option value="{{ $place->id }}" @if($search_place == $place->id) selected @endif>{{ $place->name }}</option>
+                        @endforeach
+                        
+                        </select>
+                    </div>
+                    </label>
+                </div>
+
+                <div>
+                    <input type="submit" class="btn" value="検索">
+                </div>
+            </div>
+        </form>
+    </div>
+    {{-- 検索機能ここまで --}}
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Place</th>
+            <th>About Me</th>
+        </tr>
+
+        @foreach ($items as $user)
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->category }}</td>
+            <td>{{ $user->place }}</td>
+            <td>{{ $user->about_me }}</td>
+            {{--mediaテーブルとcategoriesテーブルを結合しているので、この記述でアクセスできる--}}
+        </tr>
+        @endforeach
+    </table>
 
     <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-2 ">
 
-        @foreach ($users as $key=> $user)
+        @foreach ($items as $key=> $user)
             
 
 
